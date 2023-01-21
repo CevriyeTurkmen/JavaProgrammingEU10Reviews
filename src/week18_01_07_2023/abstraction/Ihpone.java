08247cd53cbefa056ade640728fd58500c5daf02
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-21 21:22:54 +0000

    week 18

---
+++ src/week18_01_07_2023/abstraction/Ihpone.java
@@ -0,0 +1,28 @@
+package week18_01_07_2023.abstraction;
+
+public class Ihpone extends Phone implements HasFaceTime,HasIOS,HasSiri{
+    @Override
+    public void hasFaceTime() {
+
+    }
+
+    @Override
+    public void hasIOS() {
+
+    }
+
+    @Override
+    public void hasSiri() {
+
+    }
+
+    @Override
+    void call() {
+
+    }
+
+    @Override
+    void text() {
+
+    }
+}
